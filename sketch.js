4ce56cc0fcd1aea613e925ebaad9488d49323275
marsjohnmichael <57465681+marsjohnmichael@users.noreply.github.com>
2022-02-17 11:34:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let noiseScale=0.02;
+
+function setup(){
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(0);
+  for (let x=0; x < width; x++) {
+    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+    stroke(noiseVal*255);
+    line(x, mouseY+noiseVal*80, x, height);
+  }
+}
